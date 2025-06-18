bee8a8a5202017239932edea6febd3ca518602bc
Anthony Maio <anthony21@gmail.com>
2025-06-17 20:37:10 -0400

    Updates

---
+++ pieces/cburnett/index.js
@@ -0,0 +1,22 @@
+// This file exports the SVG content for the cburnett piece set.
+// Each piece's SVG is imported as a string and then exported as part of an object.
+
+import wP from '/pieces/cburnett/wP.svg?url';
+import wR from '/pieces/cburnett/wR.svg?url';
+import wN from '/pieces/cburnett/wN.svg?url';
+import wB from '/pieces/cburnett/wB.svg?url';
+import wQ from '/pieces/cburnett/wQ.svg?url';
+import wK from '/pieces/cburnett/wK.svg?url';
+import bP from '/pieces/cburnett/bP.svg?url';
+import bR from '/pieces/cburnett/bR.svg?url';
+import bN from '/pieces/cburnett/bN.svg?url';
+import bB from '/pieces/cburnett/bB.svg?url';
+import bQ from '/pieces/cburnett/bQ.svg?url';
+import bK from '/pieces/cburnett/bK.svg?url';
+
+export const cburnettPieces = {
+  wP, wR, wN, wB, wQ, wK,
+  bP, bR, bN, bB, bQ, bK,
+};
+
+export default cburnettPieces;
